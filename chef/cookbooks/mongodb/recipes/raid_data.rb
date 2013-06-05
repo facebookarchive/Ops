f330ad0cd31f48726a6de9eeb614e45053b31278
Charity Majors <charity@parse.com>
2013-06-05 23:33:10 +0000

    make aws-sdk gem available to chef

--- chef/cookbooks/mongodb/recipes/raid_data.rb
+++ chef/cookbooks/mongodb/recipes/raid_data.rb
@@ -1,10 +1,9 @@
 include_recipe "aws"
 
-gem_package "aws-sdk" do
+chef_gem "aws-sdk" do
   version "1.3.5"
-  action :nothing
-end.run_action(:install)
-Gem.clear_paths
+  action :install
+end
 
 if node[:mongodb][:use_piops]
   create_raided_drives_from_snapshot do
