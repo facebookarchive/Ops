bd492ec2265291b601d245e0ab7e2b39c66022d4
Charity Majors <charity@parse.com>
2013-09-17 22:43:57 +0000

    name all services 'mongodb' so init script works for shards etc

--- chef/cookbooks/mongodb/definitions/mongodb.rb
+++ chef/cookbooks/mongodb/definitions/mongodb.rb
@@ -172,6 +172,7 @@ define :mongodb_instance, :mongodb_type => "mongod" , :action => [:enable, :star
 		provider Chef::Provider::Service::Upstart
     supports :status => true, :restart => true
     action service_action
+    service_name "mongodb"
     unless service_notifies.empty?
       notifies service_notifies
     end
